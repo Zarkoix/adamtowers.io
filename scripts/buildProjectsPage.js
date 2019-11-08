8bc8d4fd499198a63479eebf37bf1a516cef5edc
Adam Towers <adam.towers77@gmail.com>
2019-11-08 09:48:18 -0800

    some re-naming

--- scripts/buildProjectsPage.js
+++ scripts/buildProjectsPage.js
@@ -1,20 +1,24 @@
 const fs = require("fs");
-const projects = JSON.parse(fs.readFileSync('data/projectsData.json', 'utf8'));
-const projectTemplate = fs.readFileSync('templates/projectTemplate.html', 'utf8');
-const projectsPageTemplate = fs.readFileSync('templates/projectsPageTemplate.html', 'utf8');
+const projects = JSON.parse(fs.readFileSync("data/projects.json", "utf8"));
+const projectTemplate = fs.readFileSync("templates/project.html", "utf8");
+const projectsPageTemplate = fs.readFileSync(
+  "templates/projectsPage.html",
+  "utf8"
+);
 
 const templateArray = projects.map(project => {
-    let template = projectTemplate.slice(0);
-    Object.keys(project).forEach(key => {
-        const regex = new RegExp("{{" + key + "}}", "g");
-        template = template.replace(regex, project[key]);
-    })
-    return template;
-})
+  let template = projectTemplate.slice(0);
+  Object.keys(project).forEach(key => {
+    const regex = new RegExp("{{" + key + "}}", "g");
+    template = template.replace(regex, project[key]);
+  });
+  return template;
+});
 
-const reducer = (accumulator, currentValue) => accumulator + "\n" + currentValue;
+const reducer = (accumulator, currentValue) =>
+  accumulator + "\n" + currentValue;
 const templateText = templateArray.reduce(reducer, "");
 
 const regex = new RegExp("{{projects-list}}", "g");
 const projectsPage = projectsPageTemplate.replace(regex, templateText);
-fs.writeFileSync('projects.html', projectsPage);
+fs.writeFileSync("projects.html", projectsPage);
